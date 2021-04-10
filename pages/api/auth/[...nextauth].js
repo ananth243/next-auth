41ffe519e131af7f9d940c444c63b1344477a6d1
ananth243 <f20201797@goa.bits-pilani.ac.in>
2021-04-10 18:59:45 +0530

    First

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,36 @@
+import NextAuth from "next-auth"
+import Providers from "next-auth/providers"
+
+export default NextAuth({
+  providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+  //database: process.env.DATABASE_URL,
+  secret: process.env.SECRET,
+
+  session: {
+    //Note: `jwt` is automatically set to `true` if no database is specified.
+    jwt: true,
+    maxAge: 1 * 24 * 60 * 60, // 1 day
+  },
+  // You can define custom pages to override the built-in ones. These will be regular Next.js pages
+  // so ensure that they are placed outside of the '/api' folder, e.g. signIn: '/auth/mycustom-signin'
+  // The routes shown here are the default URLs that will be used when a custom
+  // pages is not specified for that route.
+  pages: {
+    //signin: '/auth/mycustom-signin',  // Displays signin buttons
+    // signOut: '/auth/signout', // Displays form with sign out button
+    // error: '/auth/error', // Error code passed in query string as ?error=
+    // verifyRequest: '/auth/verify-request', // Used for check email page
+    // newUser: null // If set, new users will be directed here on first sign in
+  },
+  callbacks: {
+    // async signin(user, account, profile) { return true },
+    // async redirect(url, baseUrl) { return baseUrl },
+    // async session(session, user) { return session },
+    // async jwt(token, user, account, profile, isNewUser) { return token }
+  },
+})
---
+++ pages/signIn.js
@@ -0,0 +1,5 @@
+export default function SignIn() {
+    return(
+        <a href={'/auth/mycustom-signin'}>Sign in</a>
+    )
+}
